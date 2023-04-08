c2b3a4333c76bc78a06ba7568aa55cbee679fc14
thu ha <thuharosy2003@gmai.com>
2023-04-08 07:21:04 +0700

    loading more photos

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import "./App.css";
 import Photo from "./components/Photo";
-import Toggle from "./components/Toggle.jsx";
 import { useState } from "react";
 
 function App() {
-  const [on, setOn] = useState(false);
-  const handleToggle = () => {
-    setOn((on) => !on);
-  };
   return (
     <div className="App">
-      {/* <button onClick={handleToggle} style={{ margin: "5rem" }}>
-        {on ? "Nhấn để ẩn toggle" : "Nhấn để hiện toggle"}
-      </button>
-      {on && <Toggle />} */}
       <Photo />
     </div>
   );
