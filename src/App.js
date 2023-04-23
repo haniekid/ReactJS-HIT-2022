cec76f5aeae4bbadf369a603a9a87226f2d3b515
thu ha <thuharosy2003@gmai.com>
2023-04-24 03:09:56 +0700

    using formik, create a registration form and validate data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-// import "./App.css";
-// import TestScss from "./components/scss/TestScss.jsx";
-// import Button from "./components/css-module/Button";
-// import Button2 from "./components/css-module/Button2";
-import Form1 from "./components/Form1.jsx";
-
+import SignupForm from "./components/SignupForm.jsx";
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      {/* <TestScss /> */}
-      {/* <Button /> */}
-      {/* <Button2 /> */}
-      <Form1 />
+      <SignupForm />
     </div>
   );
 }
--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,44 +1,82 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-export const SignupForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      firstName: "",
-      lastName: "",
-      email: "",
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });   
+const SignupForm = () => {
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="firstName">First Name</label>
-      <input
-        id="firstName"
-        name="firstName"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.firstName}
-      />
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        id="lastName"
-        name="lastName"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.lastName}
-      />
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={Yup.object({
+          email: Yup.string().required("Field is required!").email(),
+          password: Yup.string()
+            .required("Field is required!")
+            .matches(
+              /^(?=.*[a-zA-Z])(?=.*\d)[A-Za-z\d]{6,}$/,
+              "Password must be at least 6 characters long and contain at least one letter and one digit!"
+            ),
+        })}
+        onSubmit={async (userData) => {
+          try {
+            const response = await axios.post(
+              "https://reqres.in/api/login",
+              userData
+            );
+            console.log(response);
+            alert("Registration successful 🙌");
+          } catch (error) {
+            console.error(error);
+            if (
+              error.response &&
+              error.response.data &&
+              error.response.data.error
+            ) {
+              alert(error.response.data.error);
+            } else {
+              alert("Registration failed 😢");
+            }
+          }
+        }}
+      >
+        <Form className="form">
+          <h1>Register</h1>
+          <div className="field">
+            <label htmlFor="email">Email</label>
+            <Field
+              name="email"
+              type="text"
+              placeholder="Enter your email address"
+              className="input "
+            ></Field>
+            <div className="error">
+              <ErrorMessage name="email"></ErrorMessage>
+            </div>
+          </div>
+          <div className="field">
+            <label htmlFor="password">Password</label>
+            <Field
+              name="password"
+              type="password"
+              placeholder="Enter your password"
+              className="input"
+            ></Field>
+            <div className="error">
+              <ErrorMessage name="password"></ErrorMessage>
+            </div>
+          </div>
+          <div>
+            <button className="w-full p-4 bg-blue-600 text-white font-semibold rounded-lg">
+              Submit
+            </button>
+          </div>
+        </Form>
+      </Formik>
+    </div>
   );
 };
+
+export default SignupForm;
